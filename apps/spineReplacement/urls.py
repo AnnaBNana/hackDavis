747934f9c42f2ee0179de21aafc89c694e4ec0f7
dancinturtle <amy.giver@gmail.com>
2017-01-21 13:34:56 -0800

    result url and static

--- apps/spineReplacement/urls.py
+++ apps/spineReplacement/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^results/(?P<id>\d+)$', views.results, name="results")
 ]
--- apps/spineReplacement/views.py
+++ apps/spineReplacement/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'spineReplacement/index.html')
+
+def results(request, id):
+    context = {"id":id}
+    return render(request, 'spineReplacement/results.html', context)
