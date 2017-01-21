dee5a3e1dae2143e78d712f3b846ba713f82aea6
Anna Propas <apropas@gmail.com>
2017-01-21 13:58:35 -0800

    corrects model errors

--- apps/spineReplacement/models.py
+++ apps/spineReplacement/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Procedure(models.Model):
+    name = models.CharField(max_length=255)
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=255)
+
+class Instance(models.Model):
+    procedure = models.ForeignKey(Procedure)
+    hospital = models.ForeignKey(Hospital)
+    cost = models.DecimalField(max_digits=20, decimal_places=2)
+
+class Prerequisite(models.Model):
+    name = models.CharField(max_length=255)
+    procedure = models.ManyToManyField(Procedure, related_name="procedure")
