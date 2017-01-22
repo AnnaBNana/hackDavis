f27ce97a94cdb498377eb0a1036b0c02c802757b
Ketul Patel <kjpatel2010@gmail.com>
2017-01-22 11:54:06 -0800

    hotfix for add date

--- apps/spineReplacement/forms.py
+++ apps/spineReplacement/forms.py
@@ -4,7 +4,9 @@ from django.forms.fields import DateField
 from .models import Hospital, Instance, Procedure
 from django.contrib.admin import widgets
 
+import datetime
+
 class InstanceForm(forms.Form):
-    date = forms.DateField(widget = extras.SelectDateWidget(empty_label="Nothing", attrs={'class': 'ui fluid dropdown'}))
+    date = forms.DateField(widget = extras.SelectDateWidget(years=range(1985, datetime.date.today().year+10), empty_label="Nothing", attrs={'class': 'ui fluid dropdown'}))
 
 
