ef0e8f2fb8fbe80e9a219026218a823cb13d5bc5
Ketul Patel <kjpatel2010@gmail.com>
2017-01-21 16:17:42 -0800

    Getting data ready for results

--- apps/spineReplacement/urls.py
+++ apps/spineReplacement/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^results/(?P<id>\d+)$', views.results, name="results")
+    url(r'^results/(?P<procedure>[a-zA-Z]+)$', views.results, name="results"),
+    # url(r'^results/(?P<id>\d+)$', views.results, name="results"),
 ]
